93f411101c83859e6e69bea3690fc653a0a7f3c6
Apoc <springer.83@gmail.com>
2016-07-27 23:00:28 -0400

    Show the things to be added for the airdrop in CfgXM8

---
+++ config.cpp
@@ -0,0 +1,14 @@
+class CfgXM8
+{
+	extraApps[] = {"ExAd_APOC_Airdrop"};
+
+	class ExAd_APOC_Airdrop
+	{
+		title = "APOC Airdrop";
+		controlID = 66000;					//IDC:66000 -> 66005 || These need to be unique and out of range from each other
+		config = "ExadClient\XM8\Apps\APOC_Airdrop\config.sqf";
+		onLoad = "ExAdClient\XM8\Apps\APOC_Airdrop\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\APOC_Airdrop\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\APOC_Airdrop\onClose.sqf";
+	};
+};
