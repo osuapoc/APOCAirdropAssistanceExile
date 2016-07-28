f92f654e1606154dd71e64dcc829584b64573e36
osuapoc <springer.83@gmail.com>
2016-07-28 11:27:43 -0400

    Try this for a logo.

--- config.cpp
+++ config.cpp
@@ -6,6 +6,7 @@ class CfgXM8
 	{
 		title = "APOC Airdrop";
 		controlID = 66000;					//IDC:66000 -> 66005 || These need to be unique and out of range from each other
+		logo = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\supplydrop_ca.paa";
 		config = "ExadClient\XM8\Apps\APOC_Airdrop\config.sqf";
 		onLoad = "ExAdClient\XM8\Apps\APOC_Airdrop\onLoad.sqf";
 		onOpen = "ExAdClient\XM8\Apps\APOC_Airdrop\onOpen.sqf";
